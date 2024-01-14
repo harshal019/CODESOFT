4120ddbbaae021fd2e846f6ba0f78ed728f34101
Harshal Gharat <106591004+harshal019@users.noreply.github.com>
2024-01-14 23:47:54 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-var typed = new Typed('#element', {
-    strings: ['Web developer...','Coder...','Sports Man..!' ],
-    typeSpeed: 50,
-  });
+let string="";
+let buttons =document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML == '=')
+        {
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else  if(e.target.innerHTML == 'C')
+        {
+            string = "";
+            document.querySelector('input').value = string;
+
+        }
+        else{
+        console.log(e.target)
+        string=string + e.target.innerHTML;
+        document.querySelector('input').value = string;
+        }
+    })
+})
+
